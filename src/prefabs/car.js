ce3e18279c698f472e7eb9ea5b78b4a41c73923e
Gavin Concepcion <gbconcep@ucsc.edu>
2023-06-06 20:01:15 -0700

    Randomized Car Assets

--- src/prefabs/car.js
+++ src/prefabs/car.js
@@ -2,32 +2,24 @@ class Car extends Phaser.Physics.Arcade.Sprite {
    
     constructor(scene, lane = false) {
 
-        let carAsset = 'yellowCar';  //Phaser.Utils.Array.GetRandom(['car1', 'car2', 'car3', 'car4']);
+        let carAssets = ['orangeCar', 'yellowCar', 'greenCar', 'aquaCar', 'purpleCar', 'bluePolice' , 'blackPolice'];   
         let carSpawn = lane ? lane : Phaser.Utils.Array.GetRandom([100, 300, 500, 700]);
+
+        // Randomly select a car asset
+        let carAsset = Phaser.Utils.Array.GetRandom(carAssets);
+
         // call Phaser Physics Sprite constructor
         super(scene, carSpawn, -30, carAsset); 
         
-        this.parentScene = scene;               // maintain scene context
+        this.parentScene = scene;
 
         // set up physics sprite
-        this.parentScene.add.existing(this);    // add to existing scene, displayList, updateList
-        this.parentScene.physics.add.existing(this);    // add to physics system
-        this.setVelocityY(120);            // make it go!
+        this.parentScene.add.existing(this);
+        this.parentScene.physics.add.existing(this);
+        this.setVelocityY(120);
         this.setImmovable();
-        this.setScale(3)
-        let carColors = [0xCCCC67, 0xB3CCCC, 0xCCCC98, 0x4CCCCC, 0xFFFFFF];
-        this.tint = carColors[Math.floor(Math.random()* carColors.length)];
-        //this.angle = Math.random() * 360;
-        //this.rotateSpeed = (Math.random() * 4) - 2;
+        this.setScale(3);
     }
 
-    update() {
-
-        //this.angle += this.rotateSpeed;
-
-        // destroy paddle if it reaches the left edge of the screen
-        if(game.config.height + 100 < this.y) {
-            this.destroy();
-        }
-    }
+    // ...
 }
